0f88d7cdcc75f4fbe3305fe6dd145e54bc022a90
Ashton <53633401+AshtonMemer@users.noreply.github.com>
2024-01-19 09:14:57 -0600

    Commit v2.2.0

--- Main.cs
+++ Main.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assets.Scripts.Database;
-using Assets.Scripts.PeroTools.Commons;
-using Assets.Scripts.PeroTools.Managers;
-using Assets.Scripts.UI.Panels;
-using HarmonyLib;
+﻿using Il2CppAssets.Scripts.Database;
 using MelonLoader;
 using UnityEngine;
 
@@ -18,21 +9,28 @@ namespace QuickRestart
         private MelonPreferences_Category _category;
         private MelonPreferences_Entry _restartKeybind;
         private MelonPreferences_Entry _exitKeybind;
+        private bool _isBattleScene;
 
-        public override void OnApplicationStart()
+        /// <summary>
+        /// Initialize the MelonLoader preferences for keybinds.
+        /// </summary>
+        public override void OnInitializeMelon()
         {
-            base.OnApplicationStart();
             _category = MelonPreferences.CreateCategory("QuickRestart");
-            _restartKeybind = _category.CreateEntry<KeyCode>("Restart Key", KeyCode.Backspace);
-            _exitKeybind = _category.CreateEntry<KeyCode>("Exit Song Key", KeyCode.Delete);
+            _restartKeybind = _category.CreateEntry("Restart Key", KeyCode.Backspace);
+            _exitKeybind = _category.CreateEntry("Exit Song Key", KeyCode.Delete);
             MelonPreferences.Save();
+
+            base.OnInitializeMelon();
         }
 
-        private bool isBattleScene = false;
+        /// <summary>
+        /// Check for keybinds being pressed.
+        /// </summary>
         public override void OnUpdate()
         {
-            base.OnUpdate();
-            if (!isBattleScene) return;
+            if (!_isBattleScene) return;
+
             if (Input.GetKeyDown((KeyCode)_restartKeybind.BoxedValue))
             {
                 BattleHelper.GameRestart();
@@ -41,13 +39,18 @@ namespace QuickRestart
             {
                 BattleHelper.GameFinish();
             }
+
+            base.OnUpdate();
         }
 
+        /// <summary>
+        /// Ensure the battle scene is the active one, otherwise do not allow restarting.
+        /// </summary>
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
+            _isBattleScene = sceneName == "GameMain";
+
             base.OnSceneWasLoaded(buildIndex, sceneName);
-            if (sceneName == "GameMain") isBattleScene = true;
-            else isBattleScene = false;
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,7 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using MelonLoader;
+
+[assembly: MelonInfo(typeof(QuickRestart.Main), "QuickRestart", "2.2.0", "AshtonMemer")]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -9,9 +11,9 @@ using MelonLoader;
 [assembly: AssemblyTitle("QuickRestart")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Muse Dash Modding Community")]
 [assembly: AssemblyProduct("QuickRestart")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © AshtonMemer 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,7 +23,7 @@ using MelonLoader;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d4dac6d6-aaae-4a6f-b6de-e41475f19ef9")]
+[assembly: Guid("02bd4dca-d6ab-4a96-8f6a-0c34a34f7671")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,7 +35,5 @@ using MelonLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: MelonInfo(typeof(QuickRestart.Entry), "QuickRestart", "2.1.0", "AshtonMemer")]
-[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
